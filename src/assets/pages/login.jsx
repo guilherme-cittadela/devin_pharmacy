02b1bd9f47ec7711992a8820b809cb6df80e8a0a
Gui Cittadela <cittadela@solucoesrr.com.br>
2023-08-15 10:53:01 -0300

    criação pagina login

--- src/assets/pages/login.jsx
+++ src/assets/pages/login.jsx
@@ -1,10 +1,28 @@
-import { Campo } from "../components/campo";
-
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/system';
 function Login() {
     return ( 
-        <>
-            <Campo type="text" label="Login"/>
-            <Campo type="password" label="Senha"/>
+        <>  
+        <Box sx={{ display: "flex", flexDirection: "column", margin:"16px", justifyContent: "space-around"}}> 
+
+            <TextField 
+            id="login" 
+            label="Login" 
+            variant="outlined"
+            required />
+
+            <TextField 
+            id="password" 
+            label="Senha" 
+            variant="outlined"
+            required
+            type="password" />
+
+
+            <Button variant="contained">Entrar</Button>
+            <Button variant="text">Esqueceu sua senha?</Button>
+        </Box>
         </>
      );
 }
