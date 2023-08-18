6bc77334baedcc66b1569b17ce464c68310e79d4
Gui Cittadela <cittadela@solucoesrr.com.br>
2023-08-18 12:32:27 -0300

    alteração estilo

--- src/assets/pages/login.jsx
+++ src/assets/pages/login.jsx
@@ -1,28 +1,37 @@
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import { Box } from '@mui/system';
+import { FormContainer } from '../components/pharmacy/formStyles';
 function Login() {
     return ( 
         <>  
-        <Box sx={{ display: "flex", flexDirection: "column", margin:"16px", justifyContent: "space-around"}}> 
 
-            <TextField 
-            id="login" 
-            label="Login" 
-            variant="outlined"
-            required />
+            <Box
+                component="form"
+                sx={{
+                    '& .MuiTextField-root': { m: 1, width: '100%' },
+                }}
+                autoComplete="off"
+            >
+                <FormContainer>
+                    <TextField 
+                    id="login" 
+                    label="Login" 
+                    variant="outlined"
+                    required />
 
-            <TextField 
-            id="password" 
-            label="Senha" 
-            variant="outlined"
-            required
-            type="password" />
+                    <TextField 
+                    id="password" 
+                    label="Senha" 
+                    variant="outlined"
+                    required
+                    type="password" />
 
-
-            <Button variant="contained">Entrar</Button>
-            <Button variant="text">Esqueceu sua senha?</Button>
-        </Box>
+                    <Button variant="contained">Entrar</Button>
+                    <Button variant="text">Esqueceu sua senha?</Button>
+                </FormContainer>
+            </Box>
+        
         </>
      );
 }
